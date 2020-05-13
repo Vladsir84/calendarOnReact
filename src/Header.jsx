ad66a6254f0f1fa8a7893b9c3411e5e00ec360a5
John Doe <vperedz@gmail.com>
2020-05-13 12:18:09 +0300

    add

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,32 +3,32 @@ import React from 'react';
 const Header = () => {
     
     return (
-        <header class="header">
-        <nav class="header-navbar">
-            <button class="create-button create-event">
-                <div class="button-create-event">
-                    <svg class="create-event" width="36" height="36" viewBox="0 0 36 36">
-                        <path class="create-event" fill="#34A853" d="M16 16v14h4V20z"></path>
-                        <path class="create-event" fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
-                        <path class="create-event" fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
-                        <path class="create-event" fill="#EA4335" d="M20 16V6h-4v14z"></path>
-                        <path class="create-event" fill="none" d="M0 0h36v36H0z"></path>
+        <header className="header">
+        <nav className="header-navbar">
+            <button className="create-button create-event">
+                <div className="button-create-event">
+                    <svg className="create-event" width="36" height="36" viewBox="0 0 36 36">
+                        <path className="create-event" fill="#34A853" d="M16 16v14h4V20z"></path>
+                        <path className="create-event" fill="#4285F4" d="M30 16H20l-4 4h14z"></path>
+                        <path className="create-event" fill="#FBBC05" d="M6 16v4h10l4-4z"></path>
+                        <path className="create-event" fill="#EA4335" d="M20 16V6h-4v14z"></path>
+                        <path className="create-event" fill="none" d="M0 0h36v36H0z"></path>
                     </svg>
                 </div>
-                <span class="create-button__text create-event">Create</span>
+                <span className="create-button__text create-event">Create</span>
             </button>
-            <button class="today-button">
+            <button className="today-button">
                 Today
             </button>
-            <div class="arrow-button">
-                <button class="arrow-button__prev button-style">
-                    <i class="material-icons arrow-button__size">keyboard_arrow_left</i>
+            <div className="arrow-button">
+                <button className="arrow-button__prev button-style">
+                    <i className="material-icons arrow-button__size">keyboard_arrow_left</i>
                 </button>
-                <button class="arrow-button__next button-style">
-                    <i class="material-icons arrow-button__size ">keyboard_arrow_right</i>
+                <button className="arrow-button__next button-style">
+                    <i className="material-icons arrow-button__size ">keyboard_arrow_right</i>
                 </button>
             </div>
-            <span class="dates">
+            <span className="dates">
                 November - December 2019
             </span>
         </nav>
