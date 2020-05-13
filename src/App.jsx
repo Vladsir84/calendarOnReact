f6b45391ce8559360e77475185d1737c02ee91c6
John Doe <vperedz@gmail.com>
2020-05-13 12:38:29 +0300

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './Header'
 import Navigation from './Navigation';
+import RenderSidebar from './RenderSidebar';
 
 const App = () => {
     return (
         <>
             <Header />
             <Navigation />
+            <RenderSidebar />
         </>
     );
 };
---
+++ src/RenderSidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const RenderSidebar = () => {
+    return (
+        <section className="sidebar">
+
+            <div className="timing"></div>
+            <div className="calendar-visualization"></div>
+
+        </section>
+    );
+};
+
+export default RenderSidebar;
