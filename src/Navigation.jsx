d5c0d7b4b9e34b0e96f866772c6d2dfd661dfbb6
John Doe <vperedz@gmail.com>
2020-05-21 11:47:40 +0300

    add

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -2,48 +2,6 @@
 import React,{ Component } from 'react';
 import { generateWeekRange } from './TimeUtils.jsx'
 
-// const Navigation = () => {
-
-//     return (
-//         <nav className="days-navigation">
-//         <div className="week">
-//             <div className="day">
-//                 <div className="day-name">Sun</div>
-//                 <div className="day-number">11</div>
-//             </div>
-//             <div className="day">
-//                 <div className="day-name">Mon</div>
-//                 <div className="day-number">12</div>
-//             </div>
-//             <div className="day">
-//                 <div className="day-name">Tue</div>
-//                 <div className="day-number">13</div>
-//             </div>
-//             <div className="day">
-//                 <div className="day-name">We</div>
-//                 <div className="day-number">14</div>
-//             </div>
-//             <div className="day">
-//                 <div className="day-name">Th</div>
-//                 <div className="day-number">15</div>
-//             </div>
-//             <div className="day">
-//                 <div className="day-name">Fr</div>
-//                 <div className="day-number">16</div>
-//             </div>
-//             <div className="day">
-//                 <div className="day-name">Sat</div>
-//                 <div className="day-number">17</div>
-//             </div>
-//         </div>
-//     </nav>
-
-//     )
-// }
-
-// export default Navigation;
-
-
 
 class Navigation extends Component {
 
