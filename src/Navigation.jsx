9671247e0443dbf0627a6cae724464740bdda9c0
John Doe <vperedz@gmail.com>
2020-05-22 12:43:59 +0300

    add

--- src/Navigation.jsx
+++ src/Navigation.jsx
@@ -3,20 +3,21 @@ import React, { Component } from 'react';
 import moment from "moment";
 import { generateWeekRange } from './TimeUtils.jsx'
 
-const formatDate = date => moment(date).format("DD");
 
 class Navigation extends Component {
-
+    
     constructor(props) {
         super(props)
-
+        
         this.state = {
-            daysOfWeek: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun',]
+            daysOfWeek: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun',],
+            date: [],
         };
     }
-
+    
     render() {
-
+        
+        const formatDate = date => moment(date).format("DD");
         const daysOfNavigation = this.state.daysOfWeek.slice();
         const { monday } = this.props.monday;
         const arr = generateWeekRange(monday);
@@ -27,13 +28,16 @@ class Navigation extends Component {
             <nav className="days-navigation">
                 <div className="week">
                     {daysOfNavigation.map((day) =>
+                        <div className="day">
                         <div key={day} className="day-name">
                             {day}
-                        </div>,
-                        <div key={Math.random()} className="day-number">
-                            {numOfWeek}
                         </div>
-
+                        <div key={Math.random()} className="day-number">
+                            {formatDate(this.state.date)}
+                            {/* {numOfWeek} */}
+                         
+                        </div>
+                      </div>
                     )}
                 </div>
 
