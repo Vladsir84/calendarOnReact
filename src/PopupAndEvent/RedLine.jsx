61e2d617f7fe76ebd627b356df4b40f69227f3bf
John Doe <vperedz@gmail.com>
2020-07-27 13:15:38 +0300

    add

--- src/PopupAndEvent/RedLine.jsx
+++ src/PopupAndEvent/RedLine.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 
 const RedLine = ({ style }) => {
-  
- return (
-      <div className="redLine" style={style}>
-        <div className="redLine-round"></div>
-        <div className="redLine-line"></div>
-      </div >
-    )
-  }
+
+  return (
+    <div className="redLine" style={style}>
+      <div className="redLine-round"></div>
+      <div className="redLine-line"></div>
+    </div >
+  )
+}
 
 
 export default RedLine;
